64ad046da9bb2d66ad9837bc0cf045855f2ac93b
JJC <jjc@nlplab.cc>
2018-04-02 11:34:56 +0800

    0 is divisible by any number

--- hw4_gcd_ref.py
+++ hw4_gcd_ref.py
@@ -4,7 +4,7 @@ num_a = int(input("輸入第一個數字: "))
 num_b = int(input("輸入第二個數字: "))
 gcd = 1
 
-for i in range(gcd+1, min(num_a, num_b)+1):
+for i in range(gcd+1, max(num_a, num_b)+1):
     if num_a % i == 0 and num_b % i == 0:
         gcd = i
 
