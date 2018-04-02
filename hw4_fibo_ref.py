31f6bd93b096399168403021dd6fd7ba9ae0e5e5
JJC <jjc@nlplab.cc>
2018-04-02 10:58:33 +0800

    bug fix: variable overwrite and range

--- hw4_fibo_ref.py
+++ hw4_fibo_ref.py
@@ -3,8 +3,8 @@
 fibo_list = [0, 1]
 num = int(input("你想看費氏數列第幾個數字？ "))
 
-for i in range(2, num):
-    num = fibo_list[-1] + fibo_list[-2]
-    fibo_list.append(num)
+for i in range(2, num+1):
+    fibo_num = fibo_list[-1] + fibo_list[-2]
+    fibo_list.append(fibo_num)
 
 print('費氏數列第', num, '個數字是', fibo_list[num])
